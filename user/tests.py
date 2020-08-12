4f9c5b7f61a82372af7320ddc994d94610df9079
darkblank <darkblank1990@gmail.com>
2020-08-12 20:56:43 +0900

    :construction: 유저생성 테스트케이스 나열

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TestUserCreateAPIViewPOST(APITestCase):
+    api_url = '/users'
+
+    def setUp(self):
+        pass
+
+    def test_사용자가_회원가입_할_때_필요한_정보들을_제대로_입력하면_회원가입_201_성공(self):
+        pass
+
+    def test_사용자가_회원가입_할_때_필요한_정보들을_입력하지_않으면_회원가입_400_에러(self):
+        pass
+
+    def test_사용자가_회원가입_할_때_비밀번호_형식이_틀리면_400_에러(self):
+        pass
+
+    def test_사용자가_회원가입_할_때_username_형식이_틀리면_400_에러(self):
+        pass
+
+    def test_사용자가_회원가입_할_때_전화번호_형식이_틀리면_400_에러(self):
+        pass
+
+    def test_사용자가_회원가입_할_때_재입력_비밀번호와_그냥_비밀번호가_다르면_400_에러(self):
+        pass
