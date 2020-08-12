c0bccb3f6a89350e3e066bd3caf05327881b736b
darkblank <darkblank1990@gmail.com>
2020-08-12 20:39:15 +0900

    :card_index: user 앱 및 User 모델 추가

---
+++ user/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.gis.db import models
+
+from common.models import TimeStampModel
+
+
+class User(
+    TimeStampModel,
+    AbstractBaseUser,
+    PermissionsMixin
+):
+    username = models.CharField(
+        max_length=16,
+        unique=True,
+    )
+    name = models.CharField(
+        max_length=50,
+    )
+    email = models.EmailField(
+        unique=True,
+    )
+    phone_number = models.CharField(
+        max_length=11,
+        unique=True
+    )
+    is_active = models.BooleanField(
+        default=True,
+    )
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = '사용자'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return f'{self.username}/{self.phone_number}'
